dafb1878361f78d0d1c060420718cc30c635f5ae
sohaibsyed27 <6thsidesohaib@gmail.com>
2024-04-09 15:18:11 -0500

    finished button and changing h2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
+  var button = document.getElementById('entrybutton');
+  
+  function showText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Sohaib Syed: " + textbox.value);
+  }
+  
+  button.addEventListener('click', showText);
+}
 
 window.addEventListener('load', init);
